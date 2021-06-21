02e104265c00ac7eb953c75db8030d346366a29d
sshusshu <subin9033@gmail.com>
2021-06-21 20:35:18 +0900

    sshu: module화 하기

--- js/library/drawFace.js
+++ js/library/drawFace.js
@@ -1,79 +1,5 @@
 
-//variables
-const input = document.querySelector('input');
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d')
-const preloader = document.querySelector('.preloader');
-const alertBox = document.querySelector('.alert');
-const otherPicBtn = document.querySelector('span');
-const box1 = document.querySelector('.box1');
-const box2 = document.querySelector('.box2');
-let detecting,points,gender,mh,mw;
-
-//functions
-function init(){
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    box1.innerHTML = ''
-    box2.innerHTML = ''
-}
-function showing(target) {
-    target.classList.add('show');
-}
-function hiding(target) {
-    target.classList.remove('show');
-}
-async function resizedImg(faceapi){
-
-        const image = await faceapi.bufferToImage(input.files[0]);
-        const detect = await faceapi.detectSingleFace(image).withFaceLandmarks();
-        const point = detect.landmarks.positions;
-        const arrX =point.map(a=>a._x);
-        const arrY =point.map(a=>a._y);
-        const sx = Math.min(...arrX)-20;
-        const sy = Math.min(...arrY)-20;
-        const ex = Math.max(...arrX)+20;
-        const ey = Math.max(...arrY)+20;
-        mw = 350;
-        mh = (mw*(ey-sy))/(ex-sx);
-        canvas.width = mw;
-        canvas.height = mh;
-        canvas.style.opacity = '1'
-        ctx.drawImage(image,sx,sy,ex-sx,ey-sy,0,0,mw,mh);
-}
-async function landmarks(faceapi){
-    detecting = await faceapi
-        .detectSingleFace(canvas)
-        .withFaceLandmarks()
-        .withFaceExpressions()
-        .withAgeAndGender()
-        .withFaceDescriptor()
-    points = detecting.landmarks.positions;
-    gender = detecting.gender;
-}
-function drawingPlant(gender,mw,mh){
-    box1.style.left =`${mw}px`
-    const genderPlant = {
-        male:{
-            leaf:
-                `<svg width="${mw}" height="${mh}" viewBox="0 0 ${mw} ${mh}">
-                <path class="genderLine" d="M63.5,350.5l12-21c0,0,6-10,33-7s71,4,87-26c0,0,6-12,10-14s-16,26-54,16s-47,3-53,9s-14,15-16,14s2-1,1-7
-                s-7-42-7-42v-32c0,0,1-25-28-52s-44-31-44-31s-8-8-3-16s2,29,2,29s-1,61,36,79s45,48,45,48s4,7,11-14c0,0,5-6-2-36s33-35,54-43
-                c0,0,7-3,3-7s1,17,1,17s1,8-5,14c-3.54,3.54-12.01,12.99-15,16c-2.33,2.34-18.52,17.52-15,21c38.53,38.04,123.13-41.39,119-29
-                c-1,3-12,8-28,7c-10.18-0.64-20.89-5.35-32-3c-3.09,0.65-15,2-23,1c-5-0.63-13.4-4.98-18,0c-6.89,7.46-16.38,27.66-31.57,35.72
-                c0,0-8.43,1.28,19.57-47.72l10-19c0,0-6-12,28-23s84-52,113-84c0,0,15-2-11,7s-48,12-48,12s-31,3-63,61c0,0-17.34,25.58-12.67-5.21
-                c4.67-30.79,20.67-44.79,20.67-44.79s20-30,5-78c0,0-8-27,3-44s-7,32-25,43s-21,55-8,76s-7,75-7,75s-13.23,10-4.61-24
-                c8.61-34,1.61-56-13.39-78s-33-42-30-58s7,14-1,33c0,0-3,16,0,56c0,0-2.79,8.45,26.6,44.73c0,0,21.4,27.27,16.4,39.27"/>
-                </svg>`
-        },
-        female:{}
-    }
-    if (gender === 'female'){
-        box1.innerHTML = genderPlant.male.leaf;
-    }else{
-        box1.innerHTML = genderPlant.male.leaf;
-    }
-}
-function drawingFace(points,mw,mh){
+export default function drawingFace(box,points,mw,mh){
     const leftEye = {
         r:{
             x:points[39]._x + 7,
@@ -120,8 +46,8 @@ function drawingFace(points,mw,mh){
         x: points[26]._x,
         y: points[26]._y-100
     }
-    box2.style.top = `${mh}px`
-    box2.innerHTML =
+    box.style.top = `${mh}px`
+    box.innerHTML =
         `<svg width="${mw}" height="${mh}" viewBox="0 0 ${mw} ${mh}">    
     <path id="line"
           d="M ${points[17]._x}, ${points[17]._y-100}
@@ -420,48 +346,5 @@ function drawingFace(points,mw,mh){
            " 
             />
     </svg>`
-}
-function drawingCvs(points){
-    for(let i =0;i<points.length;i++){
-        ctx.fillText ( `${i}`,points[i]._x,points[i]._y)
-        ctx.beginPath();
-        ctx.arc(points[i]._x,points[i]._y,2,0,Math.PI*2,false)
-        ctx.stroke();
-        ctx.closePath();
-    }
-}
-
-Promise.all([
-    faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("./models"),
-    faceapi.nets.ageGenderNet.loadFromUri("./models"),
-    faceapi.nets.faceLandmark68TinyNet.loadFromUri("./models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("./models")
-]).then(start)
-
-function start(){
-    input.addEventListener('change', async function(){
-        showing(preloader);
-        try{
-            await resizedImg(faceapi);
-            await landmarks(faceapi);
-        }catch(e){
-            hiding(preloader);
-            withOutFace();
-            return
-        }
-        hiding(preloader);
-        drawingCvs(points);
-        drawingFace(points,mw,mh);
-        drawingPlant(gender,mw,mh);
-    })
-}
-
-
-function withOutFace(){
-        showing(alertBox);
-        otherPicBtn.addEventListener('click', ()=> hiding(alertBox));
-        init();
 }
 
